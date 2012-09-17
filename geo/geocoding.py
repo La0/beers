9d67f7a9a862763bed2026b6c959d993014fb518
Bastien Abadie <bastien.abadie@gmail.com>
2012-09-18 00:47:21 +0200

    Simple class for using geocoding from OSM

---
+++ geo/geocoding.py
@@ -0,0 +1,31 @@
+from urllib import urlencode
+from urllib2 import urlopen
+import simplejson as json
+
+class AddressFinder:
+  _url = "http://nominatim.openstreetmap.org/search"
+  _address = ''
+  _data = []
+  
+  def __init__(self, address, city):
+    if address == '':
+      raise Exception("Empty address")
+    self._address = '%s, %s %s' % (address, city.zipcode, city.name)
+  
+  def search(self):
+    '''
+    Search on OpenStreetMap geocoding service
+    '''
+    args = {
+      'format' : 'json',
+      'q' : self._address,
+      'countrycodes' : 'fr', # Harccoded !
+    }
+    url = '%s?%s' % (self._url, urlencode(args))
+    
+    try:
+      res = urlopen(url)
+      self._data = json.loads(res.readline())
+    except Exception, e:
+      print 'failed: %s' % (str(e))
+    return self._data
