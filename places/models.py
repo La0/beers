4b3fecbb285ade2948b6200608eff292c78b39b3
Bastien Abadie <bastien.abadie@gmail.com>
2012-09-23 17:15:30 +0200

    Add method to find subways

--- places/models.py
+++ places/models.py
@@ -30,4 +30,33 @@ class Place(Localisation):
 
   def save(self, *args, **kwargs):
     self.slug = nameize(self.name)
+
+    # Update subways
+    if self.geojson is not None:
+      self.find_subways()
+
     super(Localisation, self).save(*args, **kwargs)
+
+  def find_subways(self, max_distance = 10):
+    '''
+    Bad implementation, we should be able to limit the perimeter
+    of research...
+    '''
+    import math
+    POI_ORTHO_BASE = 1852
+
+    pos = self.get_point()
+    if pos is None:
+      return False
+    lat, lng = pos
+
+    stations = SubwayStation.objects.all() # BAD !
+    for station in stations:
+      station_lat, station_lng = station.get_point()
+      lat_diff = abs(lat - station_lat) * POI_ORTHO_BASE
+      lng_diff = abs(lng - station_lng) * POI_ORTHO_BASE
+      diff = math.ceil(math.sqrt(lat_diff**2 + lng_diff**2))
+      if diff > max_distance:
+        continue
+      self.subways.add(station)
+    self.save()
