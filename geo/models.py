799f2fec124632a437178c7ac05aeb1d6e323ffd
Bastien Abadie <bastien.abadie@gmail.com>
2013-02-02 11:47:04 +0100

    Calculate center of a geo polygon, with its radius

--- geo/models.py
+++ geo/models.py
@@ -36,6 +36,37 @@ class City(Localisation):
     self.slug = nameize(self.name)
     super(Localisation, self).save(*args, **kwargs)
 
+  def get_center(self):
+    '''
+    Calculate the center of the polygon
+    and its max radius
+    http://en.wikipedia.org/wiki/Centroid#Centroid_of_polygon
+    '''
+
+    polygon = self.get_polygon()
+    cx, cy, area = 0.0, 0.0, 0.0
+    for i in range(0, len(polygon) - 1):
+      xi, yi = polygon[i]
+      xj, yj = polygon[i+1]
+      area += (xi * yj - xj * yi) / 2.0
+      cx += (xi + xj) * (xi * yj - xj * yi)
+      cy += (yi + yj) * (xi * yj - xj * yi)
+    cx /= 6 * area
+    cy /= 6 * area
+
+    # Search the most distant point
+    def _dist(ax, ay, bx, by, R = 6371000):
+      from math import sqrt, cos, pi
+      def _torad(x):
+        return x*pi/180.0
+      ax , ay, bx, by = _torad(ax),_torad(ay),_torad(bx),_torad(by)
+      x = (by - ay) * cos((ax + bx) / 2.0)
+      y = (bx - ax)
+      return sqrt(x**2 + y**2) * R
+    radius = max([_dist(cx, cy, x, y) for x, y in polygon])
+
+    return ((cx, cy), radius)
+
 class SubwayStation(Localisation):
   name = models.CharField(max_length=255)
   lines = models.ManyToManyField('SubwayLine', through='SubwayStop', related_name='stations')
